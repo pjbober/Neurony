4a1a817d06dbdebedeec08c957f1c1e1b51b3283
pjbober <pjbober@gmail.com>
2013-05-19 14:18:51 +0200

    counter propagation

--- Kohonnen/src/counterPropagation/CounterPropagationNetwork.java
+++ Kohonnen/src/counterPropagation/CounterPropagationNetwork.java
@@ -1,5 +1,24 @@
 package counterPropagation;
 
+import feedForward.activationfunction.ActivationFunctions;
+import feedForward.feedforward.FeedForwardNet;
+import feedForward.feedforward.FeedForwardNetCreator;
+import kohonen.kohonen.KohonenNetwork;
+
 public class CounterPropagationNetwork {
+	private final KohonenNetwork kohonenLayer;
+	private final FeedForwardNet backPropagationLayer;
+
+	public CounterPropagationNetwork(int nrOfInputs, int neuronsPerRow,
+			int nrOfOutputs, int nrOfNeuronsInBackPropagation,
+			double kohonenAreaWidth) {
+		kohonenLayer = new KohonenNetwork(nrOfInputs, neuronsPerRow,
+				kohonenAreaWidth);
+		backPropagationLayer = FeedForwardNetCreator.builder(1)
+				.withNrOfInputs(kohonenLayer.getNeuronsNumber())
+				.withNrOfOutputs(nrOfOutputs)
+				.withSameNumberOfNeuronsInLayers(nrOfNeuronsInBackPropagation)
+				.withSameActivationFunction(ActivationFunctions.Linear).build();
+	}
 	
 }
